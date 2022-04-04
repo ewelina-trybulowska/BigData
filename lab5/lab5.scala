51e7685e06406d31fa8bcfe984970e98708b1912
ewelina-trybulowska <86976157+ewelina-trybulowska@users.noreply.github.com>
2022-04-04 21:05:22 +0200

    Add files via upload

---
+++ lab5/lab5.scala
@@ -0,0 +1,65 @@
+// Databricks notebook source
+Zadanie 1 
+
+Przejrzyj co jest wymagane do certyfikacji DP-203, i DP-100 
+
+https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE3VEHg 
+
+https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE3VEHg 
+
+Stwórz listę najważniejszych narzędzi analitycznych (7) i dodaj linki do dokumentacji dla Azure i AWS i poczytaj trochę. 
+
+// COMMAND ----------
+
+AZURE
+
+1. Data Factory-Azure's cloud ETL service for scale-out serverless data integration and data transformation. 
+   https://docs.microsoft.com/en-ca/azure/data-factory/
+
+2. Databricks -unified analytics platform for data analysts, data engineers, data scientists, and machine learning engineers
+  https://docs.microsoft.com/en-us/azure/databricks/
+
+3. Synapse Analytics- limitless analytics service that brings together enterprise data warehousing and Big Data analytics
+  https://docs.microsoft.com/en-ca/azure/synapse-analytics/
+
+4. Machine Learning
+   https://docs.microsoft.com/en-ca/azure/machine-learning/
+
+5. Data Lake Storage- enterprise-wide hyper-scale repository for big data analytic workloads
+    https://docs.microsoft.com/en-ca/azure/data-lake-store/
+
+6. Stream Analytics - real-time analytics and complex event-processing engine that is designed to analyze and process high volumes of fast streaming data from multiple sources simultaneously.
+  https://docs.microsoft.com/en-ca/azure/stream-analytics/
+
+7. Data Share -enables organizations to simply and securely share data with multiple customers and partners.
+  https://docs.microsoft.com/en-ca/azure/data-share/
+
+// COMMAND ----------
+
+AWS
+
+1. AWS Data Pipeline -web service that you can use to automate the movement and transformation of data
+  https://docs.aws.amazon.com/data-pipeline/
+
+2. Amazon EMR-  managed cluster platform that simplifies running big data frameworks
+  https://docs.aws.amazon.com/emr/
+
+3. Amazon RedShift- it's used for large scale data storage and analysis, and is frequently used to perform large database migrations
+  https://docs.aws.amazon.com/redshift
+
+4. AWS Glue -data integration service that makes it easy to discover, prepare, and combine data for analytics, machine learning, and application development
+  https://docs.aws.amazon.com/glue
+
+5. Amazon Athena-interactive query service that makes it easy to analyze data using standard SQL
+  https://docs.aws.amazon.com/athena/
+
+6. Amazon Kinesis-makes it easy to collect, process, and analyze video and data streams in real time
+  https://docs.aws.amazon.com/kinesis
+
+7. Amazon CloudSearch -enables search large collections of data such as web pages, document files, forum posts, or product information
+  https://docs.aws.amazon.com/cloudsearch/
+
+8.AWS SageMaker -helps data scientists and developers to prepare, build, train, and deploy high-quality machine learning models
+https://aws.amazon.com/pm/sagemaker/
+
+
